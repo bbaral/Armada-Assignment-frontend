37300c96fca851bacb73dc224c626ba05a86c2ed
Bikram Baral <bbaral@users.noreply.github.com>
2019-11-19 15:07:58 -0500

    Update grocery-list.service.ts

--- src/app/service/grocery-list.service.ts
+++ src/app/service/grocery-list.service.ts
@@ -1,7 +1,45 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {GroceryListModal} from '../model/GroceryListModal';
+import {environment} from '../../environments/environment';
+import {map, shareReplay, tap} from 'rxjs/operators';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 
 @Injectable()
 export class GroceryListService {
 
-  constructor() { }
+  private EndPoint: string = environment.EndpointAPI;
+  private Modal: { numberOfUnits: any; quantities: any; pricePerUnit: any };
+
+
+  constructor(private http: HttpClient) { }
+
+  getAllData() {
+    return this.http.get<GroceryListModal[]>(this.EndPoint + '/getAllList', httpOptions).
+    pipe(
+      map((response: any) => {
+        return response;
+      })
+    );
+  }
+
+  updateListData(incomingData: {quantities?: any, numberOfUnits?: any, pricePerUnit?: any}) {
+    return this.http.post<any>(this.EndPoint + '/updateList',
+      this.Modal = {
+      quantities: incomingData.quantities,
+        numberOfUnits: incomingData.numberOfUnits,
+        pricePerUnit: incomingData.pricePerUnit
+      }).pipe(
+        map((res: GroceryListModal) => {
+          shareReplay();
+          return res;
+        }));
+  }
 }
