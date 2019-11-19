077d1521bfc18aef13b189d1e73ae49ad04f0787
Bikram Baral <bbaral@users.noreply.github.com>
2019-11-19 15:19:06 -0500

    Create groceryListModel.ts

---
+++ src/app/model/groceryListModel.ts
@@ -0,0 +1,11 @@
+export interface GroceryListModal {
+  _id: string;
+  name: number;
+  quantities: number;
+  isAvailable: boolean;
+  description: string;
+  numberOfUnits: number;
+  pricePerUnit: number;
+  totalAmount: number;
+
+}
