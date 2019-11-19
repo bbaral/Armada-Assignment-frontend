c6cd46040102e54612064b47700533e086854a6b
Bikram Baral <bbaral@users.noreply.github.com>
2019-11-19 15:19:45 -0500

    Create edit-grocery-list.component.ts

---
+++ src/app/edit-grocery-list/edit-grocery-list.component.ts
@@ -0,0 +1,49 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {GroceryListService} from "../service/grocery-list.service";
+import {GroceryListModal} from "../model/GroceryListModal";
+
+@Component({
+  selector: 'app-edit-grocery-list',
+  templateUrl: './edit-grocery-list.component.html',
+  styleUrls: ['./edit-grocery-list.component.scss']
+})
+export class EditGroceryListComponent implements OnInit {
+
+  private EditListForm: FormGroup;
+  private tempDataHolder: GroceryListModal;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<EditGroceryListComponent>,
+    @Inject(MAT_DIALOG_DATA) public passedData: GroceryListModal,
+    private groceryListService: GroceryListService
+  ) {
+    this.tempDataHolder = passedData;
+    this.EditListForm = formBuilder.group({
+      _id: [this.tempDataHolder._id],
+      name: [this.tempDataHolder.name],
+      description: [this.tempDataHolder.description],
+      numberOfUnits: [this.tempDataHolder.numberOfUnits],
+      pricePerUnit: [this.tempDataHolder.pricePerUnit],
+      totalAmount: [this.tempDataHolder.totalAmount]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  updateCurrentRow() {
+    this.groceryListService.updateListData({
+      quantities: this.EditListForm.value.quantities,
+      numberOfUnits: this.EditListForm.value.numberOfUnits,
+      pricePerUnit: this.EditListForm.value.pricePerUnit,
+    });
+  }
+
+  cancelClick() {
+    this.dialogRef.close();
+  }
+
+}
