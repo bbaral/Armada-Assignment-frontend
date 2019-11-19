5fea913853aeb93dc954f868314b3a91c087287e
Bikram Baral <bbaral@users.noreply.github.com>
2019-11-19 15:12:17 -0500

    Update grocery-list.component.ts

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {GroceryListModal} from '../model/GroceryListModal';
+import {Observable, Subscription} from 'rxjs';
+import {GroceryListService} from '../service/grocery-list.service';
+import {MatDialog} from "@angular/material/dialog";
+import {EditGroceryListComponent} from "../edit-grocery-list/edit-grocery-list.component";
+import {MessageBoxService} from "../service/message-box.service";
 
 @Component({
   selector: 'app-grocery-list',
   templateUrl: './grocery-list.component.html',
   styleUrls: ['./grocery-list.component.scss']
 })
-export class GroceryListComponent implements OnInit {
+export class GroceryListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  displayedColumns = ['_id', 'name', 'quantities', 'description', 'numberOfUnits', 'pricePerUnit', 'totalAmount', 'action'];
+  dataSource: any = new MatTableDataSource<GroceryListModal>();
+  editRoute: string = 'updateList';
+
+  receiveDataSubscription: Subscription;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  HTMLDisplayValue: GroceryListModal[];
+
+  constructor(private groceryListService: GroceryListService,
+              private dialog: MatDialog,
+              private messageBox: MessageBoxService) {}
 
   ngOnInit() {
+    this.fetchData();
   }
 
+  fetchData() {
+    this.receiveDataSubscription = this.groceryListService.getAllData().subscribe((data: GroceryListModal[]) => {
+      console.log(data);
+      this.HTMLDisplayValue = data;
+      this.dataSource = new MatTableDataSource(this.HTMLDisplayValue);
+    });
+  }
+
+  editCurrentRow(groceryListModal: GroceryListModal, index: number) {
+    const dialogRef = this.dialog.open(EditGroceryListComponent, {
+      disableClose: true,
+      data: groceryListModal
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.fetchData();
+        this.messageBox.openDialog('done', '', 'Successfully Update', 'OK');
+      }
+    },
+     (err) => {
+      console.log(err);
+     });
+  }
+
+  ngOnDestroy(): void {
+   this.receiveDataSubscription.unsubscribe();
+  }
 }
+
+
+
+
