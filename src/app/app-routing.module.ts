c53d19a989e62a8d7267cb81cde452fffce23964
Bikram Baral <bbaral@users.noreply.github.com>
2019-11-19 16:23:37 -0500

    Update app-routing.module.ts
    router update for dialogRef

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GroceryListComponent} from './grocery-list/grocery-list.component';
-import {EditGroceryListComponent} from "./edit-grocery-list/edit-grocery-list.component";
 
 
 const routes: Routes = [
-  {path: 'grocery/getAllList', component: GroceryListComponent},
-  {path: '', redirectTo: '/grocery/getAllList', pathMatch: 'full'}
+  {path: 'home', component: GroceryListComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'grocery/getAllList', component: GroceryListComponent}
 ];
 
 @NgModule({
