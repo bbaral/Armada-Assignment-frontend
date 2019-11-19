1edee25cb168c73516839612844595be17708723
Bikram Baral <bbaral@users.noreply.github.com>
2019-11-19 15:15:34 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GroceryListComponent} from './grocery-list/grocery-list.component';
+import {EditGroceryListComponent} from "./edit-grocery-list/edit-grocery-list.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'grocery/getAllList', component: GroceryListComponent},
+  {path: '', redirectTo: '/grocery/getAllList', pathMatch: 'full'},
+  {path: 'grocery/updateList', component: EditGroceryListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
