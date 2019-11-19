e69b55b5ed537ee43392eaba438d7ed4186a5fe3
bbaral <baralbikram@hotmail.com>
2019-11-19 00:14:50 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from './material.module';
+import {GroceryListService} from './service/grocery-list.service';
+import { SidenavContainerComponent } from './sidenav-container/sidenav-container.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GroceryListComponent,
+    SidenavContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [GroceryListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
